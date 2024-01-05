8cb94d927404f78c139d3a68259482e6e820bca4
falilou14 <serignefaliloubop98@gmail.com>
2024-01-05 01:05:04 +0100

    Menu principale avec les animations et boutons , "Nom" du jeu et BackgroundImage

--- test/core/src/com/mygdx/game/MainMenu.java
+++ test/core/src/com/mygdx/game/MainMenu.java
@@ -1,8 +1,11 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Texture;
 
 public class MainMenu extends Game {
+
+   // private Texture backgroundImage ;
 
     @Override
     public void create() {
--- test/core/src/com/mygdx/game/MainMenuScreen.java
+++ test/core/src/com/mygdx/game/MainMenuScreen.java
@@ -5,16 +5,19 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.renderers.BatchTiledMapRenderer;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import jdk.javadoc.internal.doclets.formats.html.markup.Text;
 
 public class MainMenuScreen extends ScreenAdapter{
 
@@ -30,11 +33,17 @@ public class MainMenuScreen extends ScreenAdapter{
     private Table mainTable;
 
 
+    private Texture backImage;
+
+    private Label gameTitleLabel;
+
+
+
 
     public MainMenuScreen(AssetManager assetManager) {
+
         this.assetManager = assetManager;
         skin = assetManager.get(Assets.SKIN);
-
 
         // load the background "music"
         Music backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("battleThemeA.mp3"));
@@ -50,15 +59,25 @@ public class MainMenuScreen extends ScreenAdapter{
 
     @Override
     public void show() {
-        viewport = new ExtendViewport(1280,720);
+        viewport = new ExtendViewport(1200,800);
         stage = new Stage(viewport);
+        // Create the background image
+        backImage = new Texture(Gdx.files.internal("camp.png"));
+
         mainTable = new Table();
         mainTable.setFillParent(true);
+
+        gameTitleLabel = new Label("MONSTERS", skin); // le nom  du jeu
+        gameTitleLabel.setFontScale(4, 4); // Ajuste la taille de la police
+
+        mainTable.add(gameTitleLabel).colspan(2).padBottom(50); // colspan(2) pour fusionner sur deux colonnes et ajustez la disposition
+        mainTable.row();
+
         stage.addActor(mainTable);
 
         addbutton("Play");
         addbutton("Options");
-        addbutton("Credits");
+        //addbutton("Credits");
         addbutton("Quit");
 
         Gdx.input.setInputProcessor(stage); //met l'effet quand on click sur le bouton
@@ -68,18 +87,30 @@ public class MainMenuScreen extends ScreenAdapter{
 
     private TextButton addbutton(String name){
         TextButton button = new TextButton(name,skin);
-        mainTable.add(button).width(400).height(60).padBottom(20);
+        button.getLabel().setFontScale(2 ,2);
+        mainTable.add(button).width(500).height(60).padBottom(20);
         mainTable.row();
 
         return button;
 
     }
 
+    /************* NOM DU JEU ************/
+
+
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(1f,1f,15f,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+
         stage.act();
+        stage.act(Gdx.graphics.getDeltaTime());
+
+        stage.getBatch().begin();
+        // Load the background image
+        stage.getBatch().draw(backImage, 0, 0, 1200, 800);
+        stage.getBatch().end();
         stage.draw();
 
 
@@ -92,7 +123,11 @@ public class MainMenuScreen extends ScreenAdapter{
 
     @Override
     public void resize(int width, int height) {
-        viewport.update(width,height);
+
+        viewport.update(width, height, true); // Mettez à jour le viewport avec le redimensionnement
+
+
+
 
     }
 
