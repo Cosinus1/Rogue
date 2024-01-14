b27a38b51e6db098c4f3026af15226a27f6c64d9
hugobrisset <hugobrisset@orange.fr>
2024-01-14 20:38:39 +0100

    force

--- core/src/com/mygdx/game/Back/Object/Force.java
+++ core/src/com/mygdx/game/Back/Object/Force.java
@@ -4,12 +4,10 @@ public class Force {
 
     public float ForceX;
     public float ForceY;
-    public float friction;
 
-    public Force(float valueX, float valueY, float X, float Y, float friction){
+    public Force(float valueX, float valueY, float X, float Y){
         this.ForceX = valueX*X;
         this.ForceY = valueY*Y;
-        this.friction = friction;
     }
     
     public void setForceX(float X){
@@ -17,9 +15,5 @@ public class Force {
     }
     public void setForceY(float Y){
         ForceY = Y;
-    }
-    public void update(){
-        this.ForceX/=friction;
-        this.ForceY/=friction;
     }
 }
